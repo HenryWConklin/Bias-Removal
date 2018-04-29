a7919eb2df3a53f9d970890379544ec7079d62fa
Henry Conklin <henrywconklin@gmail.com>
2018-04-29 15:04:45 -0400

    changed cnn-rnn model

--- rnn_train.py
+++ rnn_train.py
@@ -1,5 +1,5 @@
 from keras.models import Model, Sequential, load_model
-from keras.layers import Input, LSTM, Dense, TimeDistributed, Softmax, RepeatVector, Bidirectional, Embedding,Dropout, Conv1D, MaxPool1D, GRU
+from keras.layers import Input, LSTM, Dense, TimeDistributed, Softmax, RepeatVector, Bidirectional, Embedding,Dropout, Conv1D, MaxPool1D, GRU, Masking
 from keras.callbacks import TensorBoard
 import numpy as np
 import parse_data
@@ -31,19 +31,21 @@ def build_conv_model():
     model.add(TimeDistributed(Dense(N_CHARS, activation='softmax')))
     return model
 
-def build_conv_rnn_model():
+def build_conv_rnn_model(max_len=None):
     model = Sequential()
     model.add(Conv1D(256, 7, padding='same', activation='relu', input_shape=(None, N_CHARS)))
     model.add(Conv1D(256, 7, padding='same', activation='relu'))
     model.add(Conv1D(256, 3, padding='same', activation='relu'))
     model.add(Conv1D(256, 3, padding='same', activation='relu'))
-    model.add(Conv1D(256, 3, padding='same', activation='relu'))
-    model.add(Conv1D(256, 3, padding='same', activation='relu'))
+    model.add(Masking(mask_value=0.0))
     model.add(Bidirectional(GRU(512, return_sequences=True, recurrent_activation='sigmoid', reset_after=True)))
     model.add(Dropout(0.5))
-    model.add(Bidirectional(GRU(512, return_sequences=True, recurrent_activation='sigmoid', reset_after=True)))
+    model.add(GRU(512, recurrent_activation='sigmoid', reset_after=True))
+    model.add(RepeatVector(max_len))
+    model.add(GRU(512, return_sequences=True, recurrent_activation='sigmoid', reset_after=True))
     model.add(Dropout(0.5))
-    model.add(TimeDistributed(Dense(N_CHARS, activation='softmax')))
+    model.add(Masking(mask_value=0.0))
+    model.add(GRU(N_CHARS, return_sequences=True, recurrent_activation='sigmoid', reset_after=True))
     return model
 
 def build_replace_model():
@@ -80,12 +82,15 @@ def build_forward_model(max_time):
 
     return model
 
-def oneHotTransform(docs, pad_to):
+def oneHotTransform(docs, pad_to, zero_pad=True):
     vecs = [np.zeros((max(len(d), pad), N_CHARS)) for d,pad in zip(docs, pad_to)]
     for i,d in enumerate(docs):
         for t,c in enumerate(d):
             # one-hot embedding
             vecs[i][t][charMap[c]] = 1
+        if not zero_pad:
+            for t in range(pad_to[i]- len(d)):
+                vecs[i][t+len(d)][0] = 1
     return vecs
 
 def wordOneHotTransform(docs, pad_to):
@@ -116,41 +121,40 @@ def buildData(path, maxrows=-1):
         data = data[:maxrows]
     in_data, out_data = zip(*data)
 
-    print(max(len(d) for d in in_data))
-    pad_len = [((max(len(d1), len(d2)) // 100 + 1) * 100) for d1,d2 in zip(in_data, out_data)]
+    # print(max(len(d) for d in in_data))
+    # pad_len = [((max(len(d1), len(d2)) // 100 + 1) * 100) for d1,d2 in zip(in_data, out_data)]
+    pad_len = max([len(d) for d in in_data] + [len(d) for d in out_data])
+    pad_len = [pad_len for i in range(len(in_data))]
 
-    in_vecs = oneHotTransform(in_data, pad_len)
-    out_vecs = oneHotTransform(out_data, pad_len)
+    in_vecs = oneHotTransform(in_data, pad_len, True)
+    out_vecs = oneHotTransform(out_data, pad_len, True)
 
     # Bin into gropus by length, in intervals of 100. Hopefully helps with runtime and weird behavior wrt padding
-    buckets = [list(filter(lambda x: i*100 < x[0].shape[0] <= (i+1)*100, zip(in_vecs, out_vecs))) for i in range(5)]
-    buckets = [list(zip(*x)) for x in buckets]
-    buckets = [(np.array(x), np.array(y)) for x,y in buckets]
-    return buckets
+    # buckets = [list(filter(lambda x: i*100 < x[0].shape[0] <= (i+1)*100, zip(in_vecs, out_vecs))) for i in range(5)]
+    # buckets = [list(zip(*x)) for x in buckets]
+    # buckets = [(np.array(x), np.array(y)) for x,y in buckets]
+    # return buckets
 
+    return np.array(in_vecs), np.array(out_vecs)
 
 def trainModel(path, model=None, epochs=3):
-    buckets = buildData(path)
+    in_vecs, out_vecs = buildData(path)
     import gc
     gc.collect()
     if model is None:
-        model = build_conv_rnn_model()
+        model = build_conv_rnn_model(out_vecs.shape[1])
         model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
     print(model.output_shape)
-    print('"' +rnn_gen.apply(model, 'I am the best string of all time') + '"')
     for i in range(epochs):
-        for in_vecs, out_vecs in buckets:
-            if len(in_vecs) < 100 or len(out_vecs) < 100:
-                continue
-            model.fit(in_vecs, out_vecs, batch_size=32, epochs=1, validation_split=0.1)
-            print('"' +rnn_gen.apply(model, 'I am the best string of all time') + '"')
+        model.fit(in_vecs, out_vecs, batch_size=25, epochs=1, validation_split=0.1)
+        print('"' +rnn_gen.apply(model, 'I am the best string of all time') + '"')
         model.save('rnn.h5')
     return model
 
 
 if __name__ == '__main__':
     #trainModel('data/train.tsv.nopunc.tsv', model=load_model('rnn.h5'), epochs=200)
-    trainModel('data/train.tsv.nopunc.tsv', epochs=200)
+    trainModel('data/train.tsv.nopunc.tsv', epochs=100)
 
 
 
