c8015c9ed902163d11f3804020a9a1e778319f4c
Henry Conklin <henrywconklin@gmail.com>
2018-04-29 00:53:13 -0400

    Check for number of columns in clean.py

--- clean.py
+++ clean.py
@@ -6,6 +6,8 @@ if __name__=="__main__":
         for line in inFile:
             # 0:title, 1:rev no, 2:revision text contains {{NPOV}} tag, 3:edit comment contains "POV", 4:editor ID, 5:revision size {minor, major, unknown}, 6:string before, 7:string after, 8:sentence before, 9:sentence after
             cols = line.strip().split('\t')
+            if len(cols) > 10:
+                continue
             cols = [x.replace('[', '').replace(']','') for x in cols]
             if cols[8] != cols[9] and cols[3] == 'true':
                 print('\t'.join(cols[8:]))
