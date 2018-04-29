25c395b0ecc3070972a709ee88ad5cd9c3a9134b
Henry Conklin <henrywconklin@gmail.com>
2018-04-28 22:45:46 -0400

    Removed {{tags}}

--- remove_punctuation.py
+++ remove_punctuation.py
@@ -1,12 +1,20 @@
 import sys
 import string
+import re
+
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as inFile:
         for line in inFile:
             line = line.split('\t')
+
+            
+            line = map(lambda s: re.sub(r'{{.*?}}', ' ', s), line)
+
+
             # Filter out punctuation
             line = map(lambda s: ''.join(map(lambda c: ' ' if c in string.punctuation else c, s)), line)
+            # Remove double spaces
             for i in range(10):
                 line = map(lambda s: s.replace('  ', ' ').strip(), line)
             line = list(line)
